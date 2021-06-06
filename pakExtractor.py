f98e6b791b0d3a937f717a5aeabef2b892170c2e
HexadecimalMantis0e <32582986+HexadecimalMantis0e@users.noreply.github.com>
2021-06-06 14:26:44 -0400

    Update for python3

--- pakExtractor.py
+++ pakExtractor.py
@@ -7,39 +7,34 @@ parser = argparse.ArgumentParser()
 parser.add_argument("pakfile")
 args = parser.parse_args()
 
-f0 = open(args.pakfile,"rb")
-
-headerMagic = struct.unpack('i', f0.read(4))[0]
+f0 = open(args.pakfile, "rb")
+headerMagic = struct.unpack('I', f0.read(4))[0]
 
 if headerMagic != 0x1234567A:
     raise ValueError("Not a pak!")
 
-print "Extracting pak..."
-
-fileCount = struct.unpack('i', f0.read(4))[0]
-
+print("Extracting pak...")
+fileCount = struct.unpack('I', f0.read(4))[0]
 f0.seek(0x18, os.SEEK_SET)
 
-for i in range(0x00, fileCount):
-    nameAddress = struct.unpack('i', f0.read(4))[0]
-    fileAddress = struct.unpack('i', f0.read(4))[0]
-    fileSize = struct.unpack('i', f0.read(4))[0]
-
+for i in range(0, fileCount):
+    nameAddress = struct.unpack('I', f0.read(4))[0]
+    fileAddress = struct.unpack('I', f0.read(4))[0]
+    fileSize = struct.unpack('I', f0.read(4))[0]
     getBack = f0.tell()
-
     f0.seek(nameAddress)
 
     nameChar = ''
     nameString = ""
 
     while nameChar != '\0':
-        nameChar = struct.unpack('c', f0.read(1))[0]
+        nameChar = struct.unpack('c', f0.read(1))[0].decode()
         nameString += nameChar
     nameString = nameString[:-1]
 
-    print nameString
-
+    print(nameString)
     filePath = os.path.join(args.pakfile[:-4], nameString)
+
     if os.path.exists(os.path.dirname(filePath)) == False:
         os.makedirs(os.path.dirname(filePath))
 
@@ -53,5 +48,5 @@ for i in range(0x00, fileCount):
     f0.seek(getBack, os.SEEK_SET)
     f0.seek(0x10, os.SEEK_CUR)
 
-print "Done!"
+print("Done!")
 f0.close()
